0f8a96af321ae297d34fe197246bbf58b6e3cfda
Ariel Mateo Anchapaxi Moreta <130407481+UdlaAriel@users.noreply.github.com>
2023-06-19 16:18:31 -0500

    Segunda version

--- Taller.c
+++ Taller.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-#define cantidad_alumnos 5
+#define cantidad_alumnos 2
 #define max 20
 
 struct alumno_structure
 {
     char nombre[20];
-    int edad[cantidad_alumnos];
+    int edad;
     char direccion[20];
     char carrera[20];
-    float promedio[cantidad_alumnos];
+    float promedio;
 
 } alumno = {"Ariel",18,"Gasca","software",10.00};
 
@@ -24,33 +24,33 @@ int main ()
         
         fflush(stdin);
         puts("Ingrese su nombre: ");
-        gets(alumno.nombre);
+        gets(alumno[i].nombre);
         
         fflush(stdin);
         puts("Ingrese su direccion: ");
-        gets(alumno.direccion);
+        gets(alumno[i].direccion);
 
         fflush(stdin);
         puts("Ingrese su carrera: ");
-        gets(alumno.carrera);
+        gets(alumno[i].carrera);
 
         puts("Ingrese su edad: ");
-        scanf("%d", &alumno.edad[i]);
+        scanf("%d", &alumno[i].edad);
 
         puts("Ingrese su promedio");
-        scanf("%f", &alumno.promedio[i]);
+        scanf("%f", &alumno[i].promedio);
     }
 
     for (int i = 0; i < cantidad_alumnos; i++)
     {
         printf("ALUMNO N# %d\n", i+1);
 
-        puts(alumno.nombre);
-        puts(alumno.direccion);
-        puts(alumno.carrera);
+        puts(alumno[i].nombre);
+        puts(alumno[i].direccion);
+        puts(alumno[i].carrera);
         
-        printf("Edad: %d\n", alumno.edad[i]);
-        printf("Promedio: %f\n", alumno.promedio[i]);
+        printf("Edad: %d\n", alumno[i].edad);
+        printf("Promedio: %.2f\n", alumno[i].promedio);
         
     }
 
